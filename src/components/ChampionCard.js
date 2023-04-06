45cb7303ef7b3bd1969dffb738bf1b8d6f734be6
Long Ha-Hoang <github@ha.mozmail.com>
2023-04-07 00:22:22 +0200

    add: fetchin for version and latest champion json

--- src/components/ChampionCard.js
+++ src/components/ChampionCard.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import Image from "next/image";
 import styles from "@/styles/ChampionGallery.module.css";
 
-export default function ChampionCart({ champion }) {
+export default function ChampionCard({ champion }) {
   const [active, setActive] = useState(false);
 
   function handleClick() {
@@ -13,14 +13,14 @@ export default function ChampionCart({ champion }) {
       <p>{champion.name}</p>
       {active ? (
         <Image
-          src={`/tiles/${champion.id}_0.jpg`}
+          src={`https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-tiles/${champion.key}/${champion.skins[0].id}.jpg`}
           height={250}
           width={250}
           alt={`${champion.id} default tile`}
         />
       ) : (
         <Image
-          src={`/tiles/${champion.id}_0.jpg`}
+          src={`https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-tiles/${champion.key}/${champion.skins[0].id}.jpg`}
           height={250}
           width={250}
           alt={`${champion.id} default tile`}
