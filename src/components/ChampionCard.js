0e88bbeed0f486d6e26a4bad90a7bdc586fafd3d
Long Ha-Hoang <github@ha.mozmail.com>
2023-04-08 22:52:27 +0200

    add: local src for images

--- src/components/ChampionCard.js
+++ src/components/ChampionCard.js
@@ -5,6 +5,9 @@ import styles from "@/styles/ChampionGallery.module.css";
 export default function ChampionCard({ champion }) {
   const [active, setActive] = useState(false);
 
+  const championTileURL = `https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-tiles/${champion.key}/${champion.skins[0].id}.jpg`;
+  const championTileLocal = `/champion-tiles/${champion.key}/${champion.skins[0].id}.jpg`;
+
   function handleClick() {
     setActive(!active);
   }
@@ -13,14 +16,14 @@ export default function ChampionCard({ champion }) {
       <p>{champion.name}</p>
       {active ? (
         <Image
-          src={`https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-tiles/${champion.key}/${champion.skins[0].id}.jpg`}
+          src={championTileLocal}
           height={250}
           width={250}
           alt={`${champion.id} default tile`}
         />
       ) : (
         <Image
-          src={`https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-tiles/${champion.key}/${champion.skins[0].id}.jpg`}
+          src={championTileLocal}
           height={250}
           width={250}
           alt={`${champion.id} default tile`}
