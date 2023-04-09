1bd61f775431f977c86db49a51aa86d7ec6df739
Long Ha-Hoang <github@ha.mozmail.com>
2023-04-09 13:34:07 +0200

    fix: dynamic img src with championFull.json data

--- src/components/ChampionCard.js
+++ src/components/ChampionCard.js
@@ -10,7 +10,7 @@ export default function ChampionCard({ champion }) {
     state.decreaseCounter,
   ]);
 
-  const championTileURL = `https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-tiles/${champion.key}/${champion.key}000.jpg`;
+  const championTileURL = `https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-tiles/${champion.key}/${champion.skins[0].id}.jpg`;
   const championTileLocal = `/champion-tiles/${champion.key}/${champion.key}000.jpg`;
 
   function handleAddChampion() {
