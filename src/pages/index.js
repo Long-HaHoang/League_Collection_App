f1573b919e9fc946ecbaed4c3e8d098b6da82ba6
Long Ha-Hoang <github@ha.mozmail.com>
2023-04-09 16:18:00 +0200

    remove unneeded Footer component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 import Head from "next/head";
 import styled from "styled-components";
 import Link from "next/link";
-import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
